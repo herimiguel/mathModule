2be7da2b8025262ab5221c3259c2468d8a32305c
Heriberto Miguel <herimiguel84@hotmail.com>
2018-07-19 18:17:52 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var module = require('./mathlib.js')();     //notice the extra invocation parentheses!
+// var math = new mathlib(); 
+
+module.add(5,6);
+module.multiply(5,6);
+module.square(5);
+module.random(5,6);
+
